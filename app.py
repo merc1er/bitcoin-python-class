fe4fecc7b3f552e18757eb2cdff0957d6424d523
Corentin MERCIER <corentin.mercier@epita.fr>
2019-06-09 00:12:20 +0700

    Created session

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session
 from bitcash import Key
+import secrets
 
 
 app = Flask(__name__)
+app.secret_key = 'c4038aaabec76362aa9f104d089936471144c97d62'
 
 HOST = '0.0.0.0'
 DEBUG = True
@@ -13,13 +15,19 @@ EVENT_BRIBE_LINK = '#'
 
 @app.route('/')
 def index():
-    key = Key()
-    wif = key.to_wif()
+    if 'user' not in session:
+        session['user'] = secrets.token_hex()
+        key = Key()
+        wif = key.to_wif()
+        # Stores the keys in the cookies
+        session['wif'] = wif
+        session['address'] = key.address
+
     return render_template(
         'index.html',
         eb_link=EVENT_BRIBE_LINK,
-        address=key.address,
-        wif=wif,
+        address=session['address'],
+        wif=session['wif'],
     )
 
 
