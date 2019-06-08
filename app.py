421d118db62c11d411bde5a7845a7bc9ab15f167
Corentin MERCIER <corentin.mercier@epita.fr>
2019-06-09 00:02:20 +0700

    BitCash Key init

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from bitcash import Key
 
 
 app = Flask(__name__)
@@ -12,7 +13,14 @@ EVENT_BRIBE_LINK = '#'
 
 @app.route('/')
 def index():
-    return render_template('index.html', eb_link=EVENT_BRIBE_LINK)
+    key = Key()
+    wif = key.to_wif()
+    return render_template(
+        'index.html',
+        eb_link=EVENT_BRIBE_LINK,
+        address=key.address,
+        wif=wif,
+    )
 
 
 if __name__ == '__main__':
